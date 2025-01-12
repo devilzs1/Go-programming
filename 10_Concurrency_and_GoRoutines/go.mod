a51095930609eb8e3d31cce65058c60ad2ce01da
devilzs1 <mohammadadil3292@gmail.com>
2025-01-12 17:01:20 +0530

    go routines, wait groups, mutex locks

---
+++ 10_Concurrency_and_GoRoutines/go.mod
@@ -0,0 +1,3 @@
+module github.com/devilzs1/goroutines
+
+go 1.23.4
--- 10_Concurrency_and_GoRoutines/main.go
+++ 10_Concurrency_and_GoRoutines/main.go
@@ -1,8 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+
+)
+
+// generally preferred as a pointer because we use them in multiple go routines
+var wg sync.WaitGroup
+var mut sync.Mutex  
+
+var signals = []string{"test"}
 
 func main() {
 	fmt.Println("Learning about Concurrency and Go Routines in Go Programming")
+
+	websiteList := []string{
+		"https://go.dev", "https://github.com/devilzs1", "https://google.com", "https://github.com",
+	}
+
+	for _, en := range websiteList {
+		go getStatusCode(en)
+		wg.Add(1)
+	}
+	wg.Wait()
+	fmt.Println(signals)
+}
+
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+
+	res, err := http.Get(endpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+	mut.Lock()
+	signals = append(signals, endpoint)
+	mut.Unlock()
+	fmt.Printf("%d status code for endpoint %s\n", res.StatusCode, endpoint)
 
 }
