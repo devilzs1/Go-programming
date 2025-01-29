361d4c42c4dcf3ec9c3f013b956674240d362145
devilzs1 <mohammadadil3292@gmail.com>
2025-01-29 21:05:42 +0530

    grpc + go init

---
+++ Projects/8_gRPC-Go/go.mod
@@ -0,0 +1,3 @@
+module github.com/devilzs1/grpc-go
+
+go 1.23.4
---
+++ Projects/8_gRPC-Go/main.go
@@ -0,0 +1,10 @@
+package main 
+
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("Learning gRPC Implementation using Go Programming")
+}
