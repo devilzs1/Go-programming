fb7b69aca168be51d853e70e1b8e15a998c9aa49
devilzs1 <mohammadadil3292@gmail.com>
2025-01-30 22:08:40 +0530

    docker + go

---
+++ Projects/9_Docker_Go/go.mod
@@ -0,0 +1,3 @@
+module github.com/devilzs1/docker-go
+
+go 1.23.4
---
+++ Projects/9_Docker_Go/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+
+func main(){
+	fmt.Println("Learning Docker integration using Go Programming")
+}
