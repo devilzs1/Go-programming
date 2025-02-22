0bc46e5b56ce22d7703f993a6415945189fcda65
devilzs1 <mohammadadil3292@gmail.com>
2025-02-22 20:23:52 +0530

    dockerize go project

--- Projects/9_Docker_Go/go.mod
+++ Projects/9_Docker_Go/go.mod
@@ -1,3 +1,3 @@
 module github.com/devilzs1/docker-go
 
-go 1.23.4
+go 1.23
--- Projects/9_Docker_Go/main.go
+++ Projects/9_Docker_Go/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
 
-
-func main(){
+func main() {
 	fmt.Println("Learning Docker integration using Go Programming")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world! Contacting from docker container api.....")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+
+// Build docker image : docker build -t go-docker-app .
+// Run the docker container : docker run -p 8080:8080 -it go-docker-app
